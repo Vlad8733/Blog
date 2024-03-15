85a747184bd1f783c29a46829561189dd510a245
Vlad8733 <125544174+Vlad8733@users.noreply.github.com>
2024-03-15 11:24:05 +0200

    Add files via upload

---
+++ Blog/views/story.view.php
@@ -0,0 +1,6 @@
+<?php require "components/head.php" ?>
+<?php require "components/navbar.php" ?>
+<h1>Story</h1>
+<h1>The Blog Revolution: A Tale of the Popular Blogging Platform <em><?php $title ?></em></h1>
+
+<?php require "components/footer.php" ?>
